602571c3b1729742ba064379e5691a26a721eeaa
Ana Junco <anajuncoescobar@gmail.com>
2021-07-08 13:51:48 -0500

    FUNCIONES

---
+++ cmake-build-debug/CMakeFiles/FUNCIONESENCLASE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FUNCIONESENCLASE.dir/main.cpp.obj"
+  "FUNCIONESENCLASE.exe"
+  "FUNCIONESENCLASE.exe.manifest"
+  "FUNCIONESENCLASE.pdb"
+  "libFUNCIONESENCLASE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FUNCIONESENCLASE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
